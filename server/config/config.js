0cf089cce0fb992b4acf153ee081a86e3973e98f
Luis Cifuetes <cifuentes_estrada@hotmail.com>
2018-07-21 19:26:40 -0600

    Login n

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,23 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB = process.env.MONGO_URI
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
+
+//================
+// Vencimiento del Token
+//=================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//================
+// SEED  de autenticacion
+//=================
+
+process.env.SEED = process.env.SEED ||'desarrollo';
